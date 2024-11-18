6fde4999d0bcadf6f7a7d805fe4fc9dc72db6586
Seva5959 <vsevolodgannik@gmail.com>
2024-11-18 23:48:38 +0500

    2 seminar> home_work_s1, make shop weares

---
+++ GB_Flask/home_work_s1/task_1.py
@@ -0,0 +1,33 @@
+# Создать базовый шаблон для интернет-магазина,
+# содержащий общие элементы дизайна (шапка, меню, подвал), и дочерние шаблоны для
+# страниц категорий товаров и отдельных товаров. Например, создать страницы «Одежда»,
+# «Обувь» и «Куртка», используя базовый шаблон.
+
+
+from flask import Flask, render_template
+from data import pants_data, footwear_data, jacket_data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def primary():
+    return render_template('primary.html')
+
+
+@app.route('/pants/')
+def def_pants():
+    return render_template('pants.html', pants=pants_data)
+
+
+@app.route('/footwear')
+def def_footwear():
+    return render_template('footwear.html')
+
+
+@app.route('/jacket')
+def def_jacket():
+    return render_template('jacket.html')
+
+
+app.run(debug=True)
