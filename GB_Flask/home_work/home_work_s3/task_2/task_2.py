5d2948dec8342c39ac1fd112278ab31b8cca993e
seva5959 <vsevolodgannik@gmail.com>
2024-12-12 23:26:38 +0500

    final home_work_s3: end

--- GB_Flask/home_work/home_work_s3/task_2/task_2.py
+++ GB_Flask/home_work/home_work_s3/task_2/task_2.py
@@ -9,11 +9,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_base_data.db'
 db.init_app(app)
 csrf = CSRFProtect(app)
 
+
 @app.cli.command('init')
 def init_db():
     db.create_all()
     print('Database initialized!')
-@app.route('/', methods=['GET','POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def auth():
     form = ValidRegist()
     if request.method == 'POST' and form.validate():
@@ -21,14 +24,14 @@ def auth():
         surn = form.sure_name.data
         pasw = form.password_us.data
         login = form.login.data
-        user = User(first_name=name,second_name=surn, login_us=login, password_us=pasw)
+        user = User(first_name=name, second_name=surn, login_us=login, password_us=pasw)
         db.session.add(user)
         db.session.commit()
         return f'Вы успешно зарегистрировались !'
     return render_template('index.html', forms=form)
 
+
 @app.route('/users')
 def get_user():
     users = User.query.all()
     return f'{users}'
-
--- GB_Flask/home_work/home_work_s3/task_2/wsgi.py
+++ GB_Flask/home_work/home_work_s3/task_2/wsgi.py
@@ -1,4 +1,3 @@
 from task_2 import app
-# не забудь прописать flask init в терминале ДАННОЙ директории, для инициализации базы данных
 if __name__ == '__main__':
     app.run(debug=True)
