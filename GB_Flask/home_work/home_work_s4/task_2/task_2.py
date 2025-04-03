7864799e5ea02fa5ce4c36df4e123d4957ced30e
seva5959 <vsevolodgannik@gmail.com>
2025-04-03 22:31:07 +0500

    HomeWork 4 seminar: сделал синхронную функцию, скачивающая все изображения

--- GB_Flask/home_work/home_work_s4/task_2/task_2.py
+++ GB_Flask/home_work/home_work_s4/task_2/task_2.py
@@ -1,20 +1,47 @@
 import os
 import re
 import requests
+import time
 
 VALID_EXTENSIONS = {"jpg", "jpeg", "png", "gif", "svg", "webp", "bmp", "tiff"}
 DIR_FOR_IMAGES = 'image_storage'
-site = 'example.html'
-domen = 'https://wikipedia.org'
+DIR_SITES = 'nabor_sites'
 
-def find_src_in_html(file_name):
-    with open(file_name, mode='r', encoding='utf-8') as f:
+
+def return_filename_domain(direction: str) -> list:
+    '''
+    Функция проходит по указанной директории состоящая из html файлов.
+    Возвращает список всех файлов и домены
+    '''
+
+    result = []
+    for filename_html in os.listdir(direction):
+        name_sites = filename_html.split('_')[1]  # Берем второй элемент 'multipocesswww', 'google', 'com.html'
+        domain = 'https://' + name_sites + '.com'
+        filename = os.path.join(direction, filename_html)
+        result.append([filename, domain])
+    return result
+
+
+def find_src_in_html(filename: str) -> list[str]:
+    '''
+    Функция обходит html файл и возвращает список всех src относящихся к img.
+    '''
+
+    with open(filename, mode='r', encoding='utf-8') as f:
         html_txt = f.read()
     src_list = re.findall(r'<img[^>]+src="([^"]+)"', html_txt)
 
     return src_list
 
-def last_prf(list_src_tag):
+
+def last_prf(list_src_anf_domain: list) -> list:
+    '''
+    Функция находит разрешение src, например: img, jpeg, svg.
+    Также возвращает список включающих 1)src, 2)само prf
+    '''
+
+    list_src_tag = list_src_anf_domain
     data = []
     for elem in list_src_tag:
         prf = elem.rsplit('.', 1)[-1].lower() if '.' in elem else ''
@@ -23,30 +50,43 @@ def last_prf(list_src_tag):
 
     return data
 
-def downloader(path_dire, src_list):
+
+def downloader(list_src_prf: list, main_dire: str, domain: str) -> None:
+    '''
+    Функция скачивает изображение в директорию, которая будет создана в указанной директории.
+    '''
+
     count = 1
-    for elem in src_list:
+    for elem in list_src_prf:
         src = elem[0]
         prf = elem[1]
-        if src[0:2] == '//': # Отрабатываю если путь начинается с //
+        if src[0:2] == '//':  # Отрабатываю если путь начинается с //
             url = 'https:' + src
-        elif src[0:2] == 'ht': # Отрабатываю если путь начинается с https или http
+        elif src[0:2] == 'ht':  # Отрабатываю если путь начинается с https или http
             url = src
-        else: # Отрабатываю если путь начинается с \
-            url = domen + src
+        else:  # Отрабатываю если путь начинается с \
+            url = domain + src
 
-        responce = requests.get(url)
-        if responce.status_code == 200:
-            file_name = f'foto_{count}.{prf}'
-            file_path = os.path.join(path_dire, file_name)
+        response = requests.get(url)
+        if response.status_code == 200:
+            # re.search: получаем имя сайта от его домена
+            dire_for_img = main_dire + re.search(r"/([^.]+)", domain).group(1)
+            os.makedirs(dire_for_img, exist_ok=True)
+            name_img = f'foto_{count}.{prf}'
+            file_path = os.path.join(dire_for_img, name_img)
             with open(file_path, mode='wb') as f:
-                f.write(responce.content)
+                f.write(response.content)
             count += 1
 
-def main(site_direction):
-    src_lst_raw = find_src_in_html(site)
-    src_list_ready = last_prf(src_lst_raw)
-    downloader(DIR_FOR_IMAGES, src_list_ready)
 
+def main():
+    start_time = time.time()
+    for elem in return_filename_domain(DIR_SITES):
+        html = elem[0]
+        domain = elem[1]
+        src_lst_raw = find_src_in_html(html)
+        src_list_ready = last_prf(src_lst_raw)
+        downloader(src_list_ready, DIR_FOR_IMAGES, domain)
+    print(f'Работа кода окончена. Было затрачено {time.time()-start_time}')
 
 main()
--- sandbox/test.py
+++ sandbox/test.py
@@ -1,39 +1,4 @@
-import os
-import requests
+import re
 
-dir_name = 'image_storage'
-file_path = os.path.join(dir_name, 'Wikipedia-logo.png')
-
-base_url = "https://www.wikipedia.org/"
-image_path = "portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png"  # Найденный путь
-image_url = base_url + image_path  # Полный URL изображения
-
-response = requests.get(image_url)  # Отправляем GET-запрос
-if response.status_code == 200: # Проверяем, что запрос успешный
-    with open(file_path, "wb") as file:
-        file.write(response.content)  # Именно метод content позволяет записывать png, mp3, ZIP, PDF
-
-# ----------------------------------------------------------------
-
-import os
-import requests
-
-dir_name = 'image_storage'
-file_path = os.path.join(dir_name, 'Wikipedia-logo.png')
-
-base_url = "https:"
-src_path = "//upload.wikimedia.org/wikipedia/commons/thumb/d/df/Wikispecies-logo.svg/17px-Wikispecies-logo.svg.png"  # Найденный путь
-image_url = base_url + src_path  # Полный URL изображения
-
-response = requests.get(image_url)  # Отправляем GET-запрос
-if response.status_code == 200: # Проверяем, что запрос успешный
-    with open(file_path, "wb") as file:
-        file.write(response.content)  # Именно метод content позволяет записывать png, mp3, ZIP, PDF
-        print('Успешно скачано')
-else:
-    print('Oшибка скачивания')
-
-
-
-
-
+name = re.search(r"/([^.]+)", "https://wikipedia.org").group(1)
+print(name)  # wikipedia
