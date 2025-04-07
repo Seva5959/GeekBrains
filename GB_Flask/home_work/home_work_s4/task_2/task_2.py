2f118f4edcc028762526715c3ea4d1d5a72b79ef
seva5959 <vsevolodgannik@gmail.com>
2025-04-07 23:07:35 +0500

    07.04.25

--- GB_Flask/home_work/home_work_s4/task_2/task_2.py
+++ GB_Flask/home_work/home_work_s4/task_2/task_2.py
@@ -1,7 +1,9 @@
 import os
 import re
-import requests
 import time
+import aiofiles
+import aiohttp
+import asyncio
 
 VALID_EXTENSIONS = {"jpg", "jpeg", "png", "gif", "svg", "webp", "bmp", "tiff"}
 DIR_FOR_IMAGES = 'image_storage'
@@ -23,13 +25,13 @@ def return_filename_domain(direction: str) -> list:
     return result
 
 
-def find_src_in_html(filename: str) -> list[str]:
+async def find_src_in_html_async(filename: str) -> list[str]:
     '''
-    Функция обходит html файл и возвращает список всех src относящихся к img.
+    Функция асинхронно обходит html файл и возвращает список всех src относящихся к img.
     '''
 
-    with open(filename, mode='r', encoding='utf-8') as f:
-        html_txt = f.read()
+    async with aiofiles.open(filename, mode='r', encoding='utf-8') as f:
+        html_txt = await f.read()
     src_list = re.findall(r'<img[^>]+src="([^"]+)"', html_txt)
 
     return src_list
@@ -51,42 +53,53 @@ def last_prf(list_src_anf_domain: list) -> list:
     return data
 
 
-def downloader(list_src_prf: list, main_dire: str, domain: str) -> None:
+async def downloader(session: aiohttp.ClientSession, src: str,
+                     prf: str, main_dire: str, domain: str, count: int) -> None:
     '''
     Функция скачивает изображение в директорию, которая будет создана в указанной директории.
     '''
 
-    count = 1
-    for elem in list_src_prf:
-        src = elem[0]
-        prf = elem[1]
-        if src[0:2] == '//':  # Отрабатываю если путь начинается с //
-            url = 'https:' + src
-        elif src[0:2] == 'ht':  # Отрабатываю если путь начинается с https или http
-            url = src
-        else:  # Отрабатываю если путь начинается с \
-            url = domain + src
+    if src.startswith('//'):  # Отрабатываю если путь начинается с //
+        url = 'https:' + src
+    elif src.startswith('ht'):  # Отрабатываю если путь начинается с https или http
+        url = src
+    else:  # Отрабатываю если путь начинается с \
+        url = domain + src
 
-        response = requests.get(url)
-        if response.status_code == 200:
+    print(f"Попытка скачивания с URL: {url}")
+    async with session.get(url) as response:
+        if response.status == 200:
             # re.search: получаем имя сайта от его домена
             dire_for_img = main_dire + re.search(r"/([^.]+)", domain).group(1)
             os.makedirs(dire_for_img, exist_ok=True)
             name_img = f'foto_{count}.{prf}'
             file_path = os.path.join(dire_for_img, name_img)
-            with open(file_path, mode='wb') as f:
-                f.write(response.content)
-            count += 1
+
+            async with aiofiles.open(file_path, mode="wb") as f:
+               await f.write(await response.read())
 
 
-def main():
+async def downloader_async(list_src_prf: list, main_dire: str, domain: str):
+    '''Данная функция напрямую связана с downloader.
+    Чтобы использовать downloader асинхронно необходим вот такой посредник'''
+    async with aiohttp.ClientSession() as session:
+        # Это list comprehension (генератор списка), а не список с 2 объектами
+        tasks = [
+            downloader(session, elem[0], elem[1], main_dire, domain, count)
+            for count, elem in enumerate(list_src_prf, start=1)
+        ]
+        await asyncio.gather(*tasks)
+
+
+async def main():
     start_time = time.time()
     for elem in return_filename_domain(DIR_SITES):
         html = elem[0]
         domain = elem[1]
-        src_lst_raw = find_src_in_html(html)
+        src_lst_raw = await find_src_in_html_async(html)
         src_list_ready = last_prf(src_lst_raw)
-        downloader(src_list_ready, DIR_FOR_IMAGES, domain)
+        await downloader_async(src_list_ready, DIR_FOR_IMAGES, domain)
     print(f'Работа кода окончена. Было затрачено {time.time()-start_time}')
 
-main()
+
+asyncio.run(main())
