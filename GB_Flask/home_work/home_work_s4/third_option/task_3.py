ac40e3caceb9d161df0092476d95e2c0914f29f4
seva5959 <vsevolodgannik@gmail.com>
2025-04-10 20:06:27 +0500

    Закончил с 3 вариантом. Сделал подробное комментирование

--- GB_Flask/home_work/home_work_s4/third_option/task_3.py
+++ GB_Flask/home_work/home_work_s4/third_option/task_3.py
@@ -8,6 +8,11 @@ site_name = 'https://www.nasa.gov/'
 dir_for_images = 'storage'
 
 def get_all_images(url: str) -> list[str]:
+    '''
+    Функция проходит по сайту и берет оттуда src. После чего преобразует в ссылку для скачивания
+
+    The function goes through the site and takes the src from there. Then it converts it into a download link.
+    '''
     # Позволяет, получить объект из которого удобно достать любой html тег
     soup = bs4.BeautifulSoup(requests.get(url).content, 'html.parser')
     urls = []
@@ -15,33 +20,53 @@ def get_all_images(url: str) -> list[str]:
     # Важно учитывать, что в асинхронном коде это может работать некорректно.
     # Лучше использовать tqdm.asyncio
     for img in tqdm.tqdm(iterable=soup.find_all('img'), desc='Извлечение изображений'):
+        # attrs превращает img (условно "str") в условно "словарь", где
+        # src и alt ключи, а остальное значение
         img_url = img.attrs.get('src')
+        # Бывают ленивые загрузки. Img загружаются непосредственно, когда пользователь доскролил
+        # Поэтому они не лежат стандартно в теге <img>
         if not img_url:
             continue
+        # Отрабатывает относительные пути по типу 1)/img/logo.png 2)/assets/img.jpg 3)images/pic.png
+        # Соединяя их с основной ссылкой для скачивания
         img_url = urllib.parse.urljoin(url, img_url)
-        try:
-            pos = img_url.index('?')
-            img_url = img_url[:pos]
-        except ValueError:
-            pass
+        # Часто сайты добавляют после разрешения файла "?" для обхода кэша
+        # В таком случае, браузер гарантировано обновит изображении, так как считает его новым, а
+        # не будет брать его из кэша
+        if '?' in img_url:
+            img_url = img_url.split('?')[0]
         if is_valid(img_url):
             urls.append(img_url)
     return urls
 
 
 def is_valid(url: str) -> bool:
+    '''
+    Функция проверяет, есть ли у ссылки домен и протокол
+
+    The function checks if the link has a domain and protocol.
+    '''
     parsed = urllib.parse.urlparse(url)
+    # Библиотека urllib позволяет удобно работать с url, разбивая его на нужные фрагменты
+    # parsed.scheme — это протокол (например: http, https, ftp)
+    # parsed.netloc — это домен (например: example.com, nasa.gov)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
 
-def download(url: str, name_dir: str) -> None:
+def download(url: str, name_dir: str, count: int) -> None:
+    '''
+    Функция скачивает изображение в указанную директорию
+
+    The function downloads the image to the specified directory
+    '''
     os.makedirs(name_dir, exist_ok=True)
     response = requests.get(url, stream=True)
+    # splitext предназначен для того, чтобы разбивать url на окончание и все остальное
+    ext = os.path.splitext(url)[1]
+    filename = os.path.join(name_dir, f'foto_{count}{ext}')
     file_size = int(response.headers.get('Content-Length', 0))
-    filename = os.path.join(name_dir, url.split('/')[-1])
-
     progress = tqdm.tqdm(response.iter_content(1024), f'Загрузка {filename}',
-        total=True, unit='B', unit_scale=True, unit_divisor=1024)
+        total=file_size, unit='B', unit_scale=True, unit_divisor=1024)
     with open(filename, mode='wb')as f:
         for data in progress.iterable:
             f.write(data)
@@ -49,9 +74,11 @@ def download(url: str, name_dir: str) -> None:
 
 
 def main(url: str, path: str) -> None:
+    count = 0
     imgs = get_all_images(url)
     for img in imgs:
-        download(img, path)
+        count += 1
+        download(img, path, count)
 
 
 main(site_name, dir_for_images)
--- sandbox/test.py
+++ sandbox/test.py
@@ -1,24 +1,14 @@
-from bs4 import BeautifulSoup
-# filename = 'index.html'
-#
-# with open(filename, 'r') as f:
-#     txt = f.read()
-#
-# soup = BeautifulSoup(txt, 'html.parser')
-# rez = soup.find_all('body')
-# print(rez)
+from urllib.parse import urlparse
 
-import tqdm
-import time
-count= 0
-numbers = [i for i in range(1, 1_000_000_000)]
+url = 'https://example.com/images/pic.jpg'
+parsed = urlparse(url)
 
-for number in tqdm.tqdm(numbers, 'Обработка чисел'):
-    count += 1
-
-
-
-
-
-
-
+print(parsed)
+# ➤ ParseResult(
+#     scheme='https',
+#     netloc='example.com',
+#     path='/images/pic.jpg',
+#     params='',
+#     query='',
+#     fragment=''
+# )
